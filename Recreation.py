c21771825d7423d8c9ed8a89564ed0c6503042f0
patchi17 <31955036+patchi17@users.noreply.github.com>
2018-10-25 07:30:28 +0200

    Update Recreation.py

--- Recreation.py
+++ Recreation.py
@@ -11,7 +11,6 @@ class RecreationSpider(scrapy.Spider):
     #def parse(self, response):
     with open("science.txt", "rt") as f:
         start_urls = [url.strip() for url in f.readlines()]
-#yield scrapy.Request(url=url, callback=self.parse_dir_contents)
         f.close()
 
     def parse(self, response):
@@ -37,31 +36,5 @@ class RecreationSpider(scrapy.Spider):
 #item['URL'] = response.request.url
 #item['Title'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]//a[contains(@target, '_blank')]//div[contains(@class, 'site-title')]//text()").extract())
 #item['Sites'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]/a[contains(@target, '_blank')]//@href").extract())
-#item['Description'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]/div[contains(@class, 'site-descr')]//text()").extract()).strip()
-        
-        
+#item['Description'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]/div[contains(@class, 'site-descr')]//text()").extract()).strip()        
 #yield item
-
-#start_urls = [
-#'http://dmoztools.net/Recreation/Drugs/Entheogens/',
-#'http://dmoztools.net/Recreation/Drugs/Psychedelics/',
-#'http://dmoztools.net/Recreation/Drugs/Cannabis/',
-#'http://dmoztools.net/Recreation/Drugs/',
-#'http://dmoztools.net/Recreation/Nudism/',
-#'http://dmoztools.net/Recreation/Guns/Airguns/',
-#'http://dmoztools.net/Recreation/Guns/Shooting/',
-#'http://dmoztools.net/Recreation/Drugs/Cannabis/Cultivation/'
-
-#]
-
-#npages = 2
-
-# This mimics getting the pages using the next button.
-#for i in range(2, npages + 2):
-# start_urls.append('http://dmoztools.net/Recreation/='+str(i)+'')
-
-#def parse(self, response):
-#for href in response.request.url
-# add the scheme, eg http://
-#url  = "https:" + href.extract()
-#yield scrapy.Request(url, callback=self.parse_dir_contents)
