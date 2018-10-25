0e67750ea4cce90c8ca52010c04eef5266372410
patchi17 <31955036+patchi17@users.noreply.github.com>
2018-10-25 07:47:35 +0200

    Update Recreation.py

--- Recreation.py
+++ Recreation.py
@@ -29,12 +29,3 @@ class RecreationSpider(scrapy.Spider):
             items.append(item)
     
         return items
-#item = RecreationItem()
-#item['MCat'] = " ".join(response.xpath("//div[contains(@class, 'current-cat recreation')]//a[contains(@class, 'breadcrumb')]//text()").extract()).split(' ', 2)[1]
-#item['SCat'] = " ".join(response.xpath("//div[contains(@class, 'current-cat recreation')]//a[contains(@class, 'breadcrumb')]//text()").extract()).split(' ', 2)[2]
-#item['CCat'] = " ".join(response.xpath("//b//text()").extract()).split(' ... ', 1)[1]
-#item['URL'] = response.request.url
-#item['Title'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]//a[contains(@target, '_blank')]//div[contains(@class, 'site-title')]//text()").extract())
-#item['Sites'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]/a[contains(@target, '_blank')]//@href").extract())
-#item['Description'] = "\n".join(response.xpath("//div[contains(@class, 'title-and-desc')]/div[contains(@class, 'site-descr')]//text()").extract()).strip()        
-#yield item
